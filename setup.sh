bd1c9a27507f8ed2d33f80a2c337a1d9ecebeb88
bearcattt <ewigglet8@gmail.com>
2025-05-14 02:20:48 +0000

    fixing static build

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ npm i
 npm run build
 
 cd dist
-wget https://bafybeidtmtattvhd2y45dbafncqmu52bxjr3kwyrftky552bpnomy2vzpu.ipfs.dweb.link/?filename=EaglercraftX_1.8_WASM-GC_Offline_Download.zip
+wget -O EaglercraftX_1.8_WASM-GC_Offline_Download.zip "https://bafybeidtmtattvhd2y45dbafncqmu52bxjr3kwyrftky552bpnomy2vzpu.ipfs.dweb.link/?filename=EaglercraftX_1.8_WASM-GC_Offline_Download.zip"
 unzip EaglercraftX_1.8_WASM-GC_Offline_Download.zip
 rm EaglercraftX_1.8_WASM-GC_Offline_Download.zip
 mv EaglercraftX_1.8_WASM-GC_Offline_Download.html index.html
-sed -i 's/<head>/<head>\<script src="index.js"><\/script>/' index.html
+sed -i 's/<head>/<head><script src="index.js"><\/script>/' index.html
 cp ../ci/_headers .
